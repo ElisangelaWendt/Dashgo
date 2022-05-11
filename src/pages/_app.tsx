1e7a056a3b252d19ea8105010fa1267f4235a845
ElisangelaWendt <61567233+ElisangelaWendt@users.noreply.github.com>
2022-05-11 16:34:28 -0300

    Finalizado

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,19 +3,27 @@ import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "../styles/theme";
 import { SidebarDrawerProvider } from "../contexts/SidebarDrawerContext";
 import { makeServer } from "../services/mirage";
+import { QueryClient, QueryClientProvider} from 'react-query'
+import {ReactQueryDevtools} from 'react-query/devtools'
+import { queryClient } from "../services/queryClient";
 
 //se o ambiente que está rodando a aplicação é de desenvolvimento
 if(process.env.NODE_ENV === 'development'){
   makeServer();
 }
 
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
+    <QueryClientProvider client={queryClient}>
     <ChakraProvider theme={theme}>
       <SidebarDrawerProvider>
         <Component {...pageProps} />
       </SidebarDrawerProvider>
     </ChakraProvider>
+
+    <ReactQueryDevtools/>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/users/index.tsx
+++ src/pages/users/index.tsx
@@ -13,69 +13,109 @@ import {
   Text,
   Checkbox,
   useBreakpointValue,
+  Spinner,
+  Link,
 } from "@chakra-ui/react";
-import Link from "next/link";
+import NextLink from "next/link";
+import { useEffect, useState } from "react";
 import { RiAddLine, RiPencilLine } from "react-icons/ri";
+
 import { Header } from "../../components/Header";
 import { Pagination } from "../../components/Pagination";
 import { Sidebar } from "../../components/Sidebar";
+import { api } from "../../services/api";
+import { UseUsers } from "../../services/hooks/useUsers";
+import { queryClient } from "../../services/queryClient";
 
-export default function UserList() {
-  const isWideVersion = useBreakpointValue({
-    base:false,
-    lg:true
+
+
+export default function UserList({users}) {
+  const [page, setPage] = useState(1)
+  const {data, isLoading, error, isFetching } = UseUsers(page, {
+    initialData: users,
   })
+
+
+  const isWideVersion = useBreakpointValue({
+    base: false,
+    lg: true,
+  });
+
+  async function handlePrefetchUser(userId: string){
+    await queryClient.prefetchQuery(['user', userId], async () => {
+      const response = await api.get(`users/${userId}`)
+
+      return response.data;
+    }, {
+      staleTime: 1000 * 60 * 10 //10 minutos
+    })
+  }
+
 
   return (
     <Box>
       <Header />
       <Flex w="100%" my="6" maxWidth={1480} mx="auto" px="6">
         <Sidebar />
-        <Box flex="1" bordeRadius={8} bg="gray.800" p="8">
+        <Box flex="1" bg="gray.800" p="8">
           <Flex mb="8" justify="space-between" align="center">
             <Heading size="lg" fontWeight="normal">
               Usuários
+              {!isLoading && isFetching && <Spinner size="sm" color="gray.500" ml="4" />}
             </Heading>
-            <Link href="/users/create" passHref>
-            <Button
-              as="a"
-              size="sm"
-              fontSize="sm"
-              colorScheme="pink"
-              leftIcon={<Icon as={RiAddLine} fontSize="20" />}
-              href="/users/create"
-            >
-              Criar novo usuário
-            </Button>
-            </Link>
-
+            <NextLink href="/users/create" passHref>
+              <Button
+                as="a"
+                size="sm"
+                fontSize="sm"
+                colorScheme="pink"
+                leftIcon={<Icon as={RiAddLine} fontSize="20" />}
+                href="/users/create"
+              >
+                Criar novo usuário
+              </Button>
+            </NextLink>
           </Flex>
 
-          <Table colorScheme="whiteAlpha">
+          {isLoading ? (
+            <Flex justify="center">
+              <Spinner/>
+            </Flex>
+          ) : error ? (
+            <Flex justify="center">
+              <Text>Falha ao obter dados dos usuários</Text>
+            </Flex>
+          ):(
+            <>
+            <Table colorScheme="whiteAlpha">
             <Thead>
               <Tr>
                 <Th px={["4", "4", "6"]} color="gray.300" width="8">
                   <Checkbox colorScheme="pink" />
                 </Th>
                 <Th>Usuário</Th>
-                {isWideVersion &&<Th>Data de cadastro</Th>}
+                {isWideVersion && <Th>Data de cadastro</Th>}
                 <Th></Th>
               </Tr>
             </Thead>
             <Tbody>
-              <Tr>
+              {data.users.map( user => {
+                return(
+                <Tr key={user.id}>
                 <Td px={["4", "4", "6"]}>
                   <Checkbox colorScheme="pink" />
                 </Td>
                 <Td>
                   <Box>
-                    <Text fontWeight="bold">Nome de usuário</Text>
+                    <Link color="purple.400" onMouseEnter={() => handlePrefetchUser(user.id)}>
+                    <Text fontWeight="bold">{user.name}</Text>
+                    </Link>
                     <Text fontSize="sm" color="gray.300">
-                      Email
+                      {user.email}
                     </Text>
                   </Box>
                 </Td>
-                {isWideVersion && <Td>04 de abril de 2022</Td>}
+                {isWideVersion && <Td>{user.createdAt}</Td>}
                 <Td>
                   <Button
                     as="a"
@@ -84,14 +124,23 @@ export default function UserList() {
                     colorScheme="orange"
                     leftIcon={<Icon as={RiPencilLine} fontSize="16" />}
                   >
-                    { isWideVersion ?  'Editar' : ''}
+                    {isWideVersion ? "Editar" : ""}
                   </Button>
                 </Td>
               </Tr>
+              )
+              })}
             </Tbody>
           </Table>
 
-          <Pagination/>
+          <Pagination 
+          totalCountOfRgisters={data.totalCount}
+          currentPage={page}
+          onPageChange={setPage}//página atual
+          />
+          </>
+
+          )}
         </Box>
       </Flex>
     </Box>
